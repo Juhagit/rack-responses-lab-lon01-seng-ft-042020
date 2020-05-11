f46ee507dd3fc2926a051c628575e900bed235a4
Juhagit <juha.ea.penttila@gmail.com>
2020-05-11 19:01:42 +0100

    Done.

---
+++ app/application.rb
@@ -0,0 +1,19 @@
+class Application
+
+    def call(env)
+      resp = Rack::Response.new
+  
+      num_1 = Time.now
+
+
+      if num_1.hour < 12
+        resp.write "Good Morning!"
+        
+      else
+        resp.write "Good Afternoon!"
+      end
+  
+      resp.finish
+    end
+  
+  end
